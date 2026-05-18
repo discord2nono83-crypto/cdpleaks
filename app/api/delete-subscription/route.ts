4c5ae4d36502a82818270655e3b6fcf4d95bb854
Discordgit config --global user.email discord2nono83@gmail.comgit add .git commit -mgit config --global user.name Discord <discord2nono83@gmail.com>
2026-05-18 19:11:48 +0200

    fix delete subscrition route

--- app/api/delete-subscription/route.ts
+++ app/api/delete-subscription/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import { supabaseAdmin } from "@/lib/supabaseAdmin";
+
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
   try {
     const { id } = await req.json();
@@ -11,11 +14,18 @@ export async function POST(req: Request) {
 
     if (error) {
       console.error("DELETE ERROR:", error.message);
-      return NextResponse.json({ error: error.message }, { status: 500 });
+
+      return NextResponse.json(
+        { error: error.message },
+        { status: 500 }
+      );
     }
 
     return NextResponse.json({ success: true });
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
   }
 }
